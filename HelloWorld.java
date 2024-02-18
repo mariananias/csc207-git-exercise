5ee073fc8a1892093621d29f287d9d5558cf6de0
mariananias <157740466+mariananias@users.noreply.github.com>
2024-02-17 22:09:37 -0600

    Updated HelloWorld (new line)

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, GitHub (updated)!");
+    pen.println ("New change!")
     pen.flush();
   } // main(String[])
 } // class HelloWorld
